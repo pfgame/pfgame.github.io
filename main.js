c222f14d7454c70236a9e2943c6a8da47b536a62
pfgame <juanvilch1980@gmail.com>
2018-06-10 15:52:49 -0400

    add randomInt function

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+/*
 
+TABLE OF CONTENTS
+A. Utilities
+  1. randomInt function: takes two numbers and returns a random int
+  2.
+
+*/
+
+
+// A. Utilities
+
+// 1. randomInt
+
+function randomInt(min, max) {
+  //inclusive on both sides
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
